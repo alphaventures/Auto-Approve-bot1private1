1f383881a4dfa6c9259c5b1b32f7c71bf670c361
Anky Ron <99786595+alphaventures@users.noreply.github.com>
2023-03-04 18:21:17 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("17572104"))
-    API_HASH = getenv("db57dc05bff345180fa5bd0a5d74c3e0")
-    BOT_TOKEN = getenv("5855874254:AAH_rUpg9PeeX-KTXZ4OD7ZTTrhyKJfeU0I")
+    API_ID = int(getenv("API_ID"))
+    API_HASH = getenv("API_HASH")
+    BOT_TOKEN = getenv("BOT_TOKEN")
     FSUB = getenv("approvebotfree")
     CHID = int(getenv("1732001513"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("mongodb+srv://ankynoda:ankynoda@cluster.sc0egiu.mongodb.net/?retryWrites=true&w=majority", "")
+    MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
